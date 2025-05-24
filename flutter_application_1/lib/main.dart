07cb4d04c8c2dc500d61738ece109423cd5a0ec8
Luiz Felipe Hildebrant <luiz.felipe@webfloat.com.br>
2025-05-24 15:13:44 -0300

    Firebase

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/meu_aplicativo.dart';
-void main(){
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MeuAplicativo());
 }
